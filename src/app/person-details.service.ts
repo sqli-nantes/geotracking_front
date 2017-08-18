39bcd203355c8165d157bfa874c1444eadd2a45c
Johann <johann@latitude.ubuntu>
2017-08-18 17:46:07 +0200

    Add bootstrap service

---
+++ src/app/person-details.service.ts
@@ -0,0 +1,15 @@
+export const bootstrap = [
+    {
+        name: "Jeanne",
+        forename: "Dupont"
+    }, {
+        name: "Pierre",
+        forename: "Art"
+    }, {
+        name: "Jean",
+        forename: "Diu"
+    }, {
+        name: "Luc",
+        forename: "Cert"
+    }
+]
